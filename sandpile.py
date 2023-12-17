52d536ea88ddb692d1ba43a9f7765658c389def0
leotolstoy <leomed97@gmail.com>
2023-12-16 17:15:40 -0800

    reverted sandpile to not reset all cumulative scores to zero if agent falls off grid to improve rl training

--- sandpile.py
+++ sandpile.py
@@ -74,7 +74,7 @@ class Sandpile():
             # check agent position 
             if agent.is_in_game() and not self.check_agent_is_in_grid(agent):
                 agent.remove_agent_from_game()
-                self.agent_rewards_step[i] = -agent.get_cumulative_score()
+                # self.agent_rewards_step[i] = -agent.get_cumulative_score()
                 # agent.append_reward(-agent.get_cumulative_score())
                 agent.append_reward(0)
                 # agent_rewards_step[i] -= 100
@@ -113,7 +113,7 @@ class Sandpile():
                     # self.agent_rewards.append(reward)
             else:
                 # self.agent_rewards.append(-100)
-                self.agent_rewards_step[i] = -agent.get_cumulative_score()
+                # self.agent_rewards_step[i] = -agent.get_cumulative_score()
                 pass
 
             
