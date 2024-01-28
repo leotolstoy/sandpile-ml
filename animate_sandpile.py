1ab3662891271e6d7ff6f405a266f94ac4f464cb
leotolstoy <leomed97@gmail.com>
2024-01-27 16:48:34 -0800

    removed loglog plots from animation scripts

--- animate_sandpile.py
+++ animate_sandpile.py
@@ -14,7 +14,7 @@ N_grid = 10 #number of cells per side
 N_tick_step = 1
 
 MAXIMUM_GRAINS = 4
-N_runs = 200
+N_runs = 500
 
 I = 0
 fig = plt.figure()
@@ -134,32 +134,7 @@ anim = animation.FuncAnimation(fig, animate, frames=frames, interval=interval, b
 if DO_EXPORT_ANIM:
     anim.save('animation.gif', writer='imagemagick', fps=2)
 
-#print final grid
-sandpile.print_grid()
 
-# get avalance sizes
-avalanche_sizes = np.array(sandpile.avalanche_sizes)
-
-#plot histogram and loglog
-bins = 10
-hist_vals, x_recon = np.histogram(avalanche_sizes, bins=bins, density=True)
-
-fig_hist, axs_hist = plt.subplots(2,1)
-axs_hist[0].hist(avalanche_sizes,bins=bins, density=True)
-axs_hist[1].loglog(x_recon[:-1],hist_vals,color='r',marker='o')
-
-# img = axs.imshow(grid)
-
-
-#manually log avalanche sizes and then plot histogram
-log_avalanche_sizes = np.log10(avalanche_sizes)
-
-bins = 10
-hist_vals, x_recon = np.histogram(log_avalanche_sizes, bins=bins, density=True)
-
-fig_hist, axs_hist = plt.subplots(2,1)
-axs_hist[0].hist(log_avalanche_sizes,bins=bins, density=True)
-axs_hist[1].plot(np.log10(x_recon[:-1]),hist_vals,color='r',marker='o')
 plt.show()
 
 
